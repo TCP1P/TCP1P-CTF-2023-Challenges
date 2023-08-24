3dcd516f7664d9dc22d09b913e1520245ce8d1e0
dimasma0305 <dimasmaulana0305@gmail.com>
2023-08-24 09:54:52 +0800

    fix template

--- .ctf/deploy.sh
+++ .ctf/deploy.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Find directories containing docker-compose.yaml or docker-compose.yml files
-export directories=$(find . -maxdepth 2 -type f -name "docker-compose.yaml" -o -name "docker-compose.yml" | xargs -n 1 dirname | sort -u)
+export directories=$(find . -maxdepth 3 -type f \( -name "docker-compose.yaml" -o -name "docker-compose.yml" \) -exec dirname {} \; | sort -u)
+IFS=$'\n'
 
 for dir in $directories; do
     if [[ "$dir" =~ \./\..* ]]; then
@@ -17,3 +18,5 @@ for dir in $directories; do
         sudo docker-compose up --detach
     )&
 done
+
+wait
