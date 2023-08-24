0393bbb0c3dd98275bb2d30c99bde88aa030ffc4
dimasma0305 <dimasmaulana0305@gmail.com>
2023-08-24 09:30:06 +0800

    chang to deploy script

--- .ctf/deploy.sh
+++ .ctf/deploy.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 set -e
 
-export directories=$(find . -maxdepth 2 -type d)
-IFS=$'\n'
+# Find directories containing docker-compose.yaml or docker-compose.yml files
+export directories=$(find . -maxdepth 2 -type f -name "docker-compose.yaml" -o -name "docker-compose.yml" | xargs -n 1 dirname | sort -u)
+
 for dir in $directories; do
     if [[ "$dir" =~ \./\..* ]]; then
         continue
     fi
-    if [ "$dir" != ".." ] && [[ "$dir" =~ \./.*/ ]]; then
-        echo "Processing directory: $dir"
-cat <<EOF | sudo bash
-    cd "$dir"
-    docker-compose up --detach
-EOF
-    fi
+
+    echo "Processing directory: $dir"
+
+    # Change to the directory and run docker-compose up --detach
+    (
+        cd "$dir"
+        sudo docker-compose up --detach
+    )&
 done
